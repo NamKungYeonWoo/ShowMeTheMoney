06045baa4c05723e221b6d7640b61d18f6dbcd50
uije <zettafantasy@gmail.com>
2017-02-11 23:39:18 +0900

    feat: add barchart activity example

---
+++ app/src/main/java/com/zettafantasy/visualhousekeepingbook/custom/MyAxisValueFormatter.java
@@ -0,0 +1,24 @@
+package com.zettafantasy.visualhousekeepingbook.custom;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by uije on 17. 2. 11.
+ */
+public class MyAxisValueFormatter implements IAxisValueFormatter
+{
+
+    private DecimalFormat mFormat;
+
+    public MyAxisValueFormatter() {
+        mFormat = new DecimalFormat("###,###,###,##0.0");
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        return mFormat.format(value) + " $";
+    }
+}
