1fb0e6b9d66635b29a14ccbf4835f3c94efdc0b9
uije <zettafantasy@gmail.com>
2017-02-16 10:15:42 +0900

    feat: 돈입력 창 dialog ->fragement activity로 변

--- app/src/main/java/com/zettafantasy/visualhousekeepingbook/MainActivity.java
+++ app/src/main/java/com/zettafantasy/visualhousekeepingbook/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +29,11 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
-                MoneyEntryDialogFragment fragment = new MoneyEntryDialogFragment();
-                fragment.show(getFragmentManager(), "TAG");
                 //Intent startBarActivity = new Intent(MainActivity.this, BarChartActivity.class);
                 //startActivity(startBarActivity);
+
+                Intent startMoneyEntryActivity = new Intent(MainActivity.this, MoneyEntryActivity.class);
+                startActivity(startMoneyEntryActivity);
             }
         });
     }
