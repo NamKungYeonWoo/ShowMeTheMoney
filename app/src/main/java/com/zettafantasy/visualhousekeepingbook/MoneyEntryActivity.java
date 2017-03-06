09564dc61e46de878c5029ffd065fb849e355a1c
uije <zettafantasy@gmail.com>
2017-03-06 23:37:41 +0900

    feat: 다이얼패드 입력연

--- app/src/main/java/com/zettafantasy/visualhousekeepingbook/MoneyEntryActivity.java
+++ app/src/main/java/com/zettafantasy/visualhousekeepingbook/MoneyEntryActivity.java
@@ -1,20 +1,72 @@
 package com.zettafantasy.visualhousekeepingbook;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MoneyEntryActivity extends AppCompatActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_entry);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_money_entry);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_entry);
         setSupportActionBar(myToolbar);
+
         ActionBar supportActionBar = getSupportActionBar();
         supportActionBar.setDisplayShowTitleEnabled(false);
         supportActionBar.setDisplayHomeAsUpEnabled(true);
+
+        initViewPager();
+    }
+
+    public void initViewPager() {
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout_main);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager_main);
+
+        List<String> titles = new ArrayList<>();
+        titles.add(getString(R.string.expense));
+        titles.add(getString(R.string.income));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(0)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(1)));
+        
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new MoneyEntryFragment());
+        fragments.add(new MoneyEntryFragment());
+
+        mViewPager.setOffscreenPageLimit(1);
+
+        FragmentAdapter mFragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
+        mViewPager.setAdapter(mFragmentAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    public void onNumberClick(View view) {
     }
 }
--- app/src/main/java/com/zettafantasy/visualhousekeepingbook/MoneyEntryFragment.java
+++ app/src/main/java/com/zettafantasy/visualhousekeepingbook/MoneyEntryFragment.java
@@ -1,9 +1,12 @@
 package com.zettafantasy.visualhousekeepingbook;
 
 import android.app.DatePickerDialog;
+import android.icu.text.DecimalFormat;
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -26,14 +30,39 @@ import butterknife.Unbinder;
 
 public class MoneyEntryFragment extends Fragment {
 
+    private static final String TAG = MoneyEntryFragment.class.getSimpleName();
     private Unbinder unbinder;
 
     @BindView(R.id.tvPickDate)
     TextView tvPickDate;
-//    @BindView(R.id.sEntryType)
-//    Spinner sEntryType;
-//    @BindView(R.id.etMoney)
-//    EditText etMoney;
+    @BindView(R.id.tvNumber1)
+    TextView tvNumber1;
+    @BindView(R.id.tvNumber2)
+    TextView tvNumber2;
+    @BindView(R.id.tvNumber3)
+    TextView tvNumber3;
+    @BindView(R.id.tvNumber4)
+    TextView tvNumber4;
+    @BindView(R.id.tvNumber5)
+    TextView tvNumber5;
+    @BindView(R.id.tvNumber6)
+    TextView tvNumber6;
+    @BindView(R.id.tvNumber7)
+    TextView tvNumber7;
+    @BindView(R.id.tvNumber8)
+    TextView tvNumber8;
+    @BindView(R.id.tvNumber9)
+    TextView tvNumber9;
+    @BindView(R.id.tvNumber0)
+    TextView tvNumber0;
+    @BindView(R.id.tvNumber00)
+    TextView tvNumber00;
+    @BindView(R.id.tvNumberC)
+    TextView tvNumberC;
+    @BindView(R.id.tvAmount)
+    TextView tvAmount;
+
+    private final DecimalFormat currencyFormat = new DecimalFormat("#,###,###");
 
     @Nullable
     @Override
@@ -42,6 +71,21 @@ public class MoneyEntryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_money_entry, container, false);
         unbinder = ButterKnife.bind(this, view);
 
+        initNumberPad();
+        initDatePicker();
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.expense_type, R.layout.custom_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        //sEntryType.setAdapter(adapter);
+
+        return view;
+    }
+
+    private void initDatePicker() {
         final Calendar rightNow = Calendar.getInstance();
         String formattedDate = String.format(
                 Locale.KOREAN,
@@ -70,16 +114,80 @@ public class MoneyEntryFragment extends Fragment {
                         .show();
             }
         });
+    }
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.expense_type, R.layout.custom_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        //sEntryType.setAdapter(adapter);
+    private void initNumberPad() {
+        View.OnClickListener onClickNumberPadListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String amount = tvAmount.getText().toString();
 
-        return view;
+                switch (v.getId()) {
+                    case R.id.tvNumber1:
+                        amount += getContext().getString(R.string.number_1);
+                        break;
+                    case R.id.tvNumber2:
+                        amount += getContext().getString(R.string.number_2);
+                        break;
+                    case R.id.tvNumber3:
+                        amount += getContext().getString(R.string.number_3);
+                        break;
+                    case R.id.tvNumber4:
+                        amount += getContext().getString(R.string.number_4);
+                        break;
+                    case R.id.tvNumber5:
+                        amount += getContext().getString(R.string.number_5);
+                        break;
+                    case R.id.tvNumber6:
+                        amount += getContext().getString(R.string.number_6);
+                        break;
+                    case R.id.tvNumber7:
+                        amount += getContext().getString(R.string.number_7);
+                        break;
+                    case R.id.tvNumber8:
+                        amount += getContext().getString(R.string.number_8);
+                        break;
+                    case R.id.tvNumber9:
+                        amount += getContext().getString(R.string.number_9);
+                        break;
+                    case R.id.tvNumber0:
+                        amount += getContext().getString(R.string.number_0);
+                        break;
+                    case R.id.tvNumber00:
+                        amount += getContext().getString(R.string.number_00);
+                        break;
+                    case R.id.tvNumberC:
+                        amount = getContext().getString(R.string.number_0);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("invalid number id " + v.getId());
+                }
+
+                // currency formatting
+                try {
+                    tvAmount.setText(
+                            currencyFormat.format(
+                                    Long.valueOf(amount.replaceAll("\\D", ""))));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e(TAG, e.getLocalizedMessage());
+                    tvAmount.setText(getContext().getString(R.string.number_0));
+                }
+            }
+        };
+
+        tvNumber1.setOnClickListener(onClickNumberPadListener);
+        tvNumber2.setOnClickListener(onClickNumberPadListener);
+        tvNumber3.setOnClickListener(onClickNumberPadListener);
+        tvNumber4.setOnClickListener(onClickNumberPadListener);
+        tvNumber5.setOnClickListener(onClickNumberPadListener);
+        tvNumber6.setOnClickListener(onClickNumberPadListener);
+        tvNumber7.setOnClickListener(onClickNumberPadListener);
+        tvNumber8.setOnClickListener(onClickNumberPadListener);
+        tvNumber9.setOnClickListener(onClickNumberPadListener);
+        tvNumber0.setOnClickListener(onClickNumberPadListener);
+        tvNumber00.setOnClickListener(onClickNumberPadListener);
+        tvNumberC.setOnClickListener(onClickNumberPadListener);
     }
 
     @Override
