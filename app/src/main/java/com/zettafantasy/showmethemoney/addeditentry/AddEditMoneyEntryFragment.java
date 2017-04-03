d5a345721857413ce9dd42d48b0b15351a79f84b
uije <zettafantasy@gmail.com>
2017-04-03 20:32:40 +0900

    chore : 키패드 UI 개선

--- app/src/main/java/com/zettafantasy/showmethemoney/addeditentry/AddEditMoneyEntryFragment.java
+++ app/src/main/java/com/zettafantasy/showmethemoney/addeditentry/AddEditMoneyEntryFragment.java
@@ -22,22 +22,40 @@ import butterknife.Unbinder;
  */
 public class AddEditMoneyEntryFragment extends Fragment implements AddEditMoneyEntryContract.View {
     public static final String ARGUMENT_EDIT_ENTRY_ID = "ENTRY_EDIT_ID";
-
-    private Unbinder unbinder;
-
     @BindView(R.id.vgDialPad)
     ViewGroup mDialPad;
     @BindView(R.id.tvAmount)
     TextView mTvAmount;
     @BindView(R.id.tvNumber1)
-    TextView mTvNumber1;
+    View mTvNumber1;
     @BindView(R.id.tvNumber2)
-    TextView mTvNumber2;
+    View mTvNumber2;
     @BindView(R.id.tvNumber3)
-    TextView mTvNumber3;
-
+    View mTvNumber3;
+    @BindView(R.id.tvNumber4)
+    View mTvNumber4;
+    @BindView(R.id.tvNumber5)
+    View mTvNumber5;
+    @BindView(R.id.tvNumber6)
+    View mTvNumber6;
+    @BindView(R.id.tvNumber7)
+    View mTvNumber7;
+    @BindView(R.id.tvNumber8)
+    View mTvNumber8;
+    @BindView(R.id.tvNumber9)
+    View mTvNumber9;
+    @BindView(R.id.tvNumber00)
+    View mTvNumber00;
+    @BindView(R.id.tvNumber0)
+    View mTvNumber0;
+    @BindView(R.id.tvNumberC)
+    View mTvNumberC;
+    private Unbinder unbinder;
     private long mAmount = 0;
 
+    public static AddEditMoneyEntryFragment newInstance() {
+        return new AddEditMoneyEntryFragment();
+    }
 
     @Override
     public void setPresenter(AddEditMoneyEntryContract.Presenter presenter) {
@@ -89,10 +107,6 @@ public class AddEditMoneyEntryFragment extends Fragment implements AddEditMoneyE
 
     }
 
-    public static AddEditMoneyEntryFragment newInstance() {
-        return new AddEditMoneyEntryFragment();
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -112,34 +126,53 @@ public class AddEditMoneyEntryFragment extends Fragment implements AddEditMoneyE
     private void initNumberPad() {
         mTvAmount.setText(StringUtils.makeNumberComma(mAmount));
 
-        mTvNumber1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAmount = (mAmount == 0 ? 1 : mAmount * 10 + 1);
-                mTvAmount.setText(StringUtils.makeNumberComma(mAmount));
-            }
-        });
-
-        mTvNumber2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAmount = (mAmount == 0 ? 2 : mAmount * 10 + 2);
-                mTvAmount.setText(StringUtils.makeNumberComma(mAmount));
-            }
-        });
-
-        mTvNumber3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAmount = (mAmount == 0 ? 3 : mAmount * 10 + 3);
-                mTvAmount.setText(StringUtils.makeNumberComma(mAmount));
-            }
-        });
+        mTvNumber1.setOnClickListener(new NumberPadClickListener(1));
+        mTvNumber2.setOnClickListener(new NumberPadClickListener(2));
+        mTvNumber3.setOnClickListener(new NumberPadClickListener(3));
+        mTvNumber4.setOnClickListener(new NumberPadClickListener(4));
+        mTvNumber5.setOnClickListener(new NumberPadClickListener(5));
+        mTvNumber6.setOnClickListener(new NumberPadClickListener(6));
+        mTvNumber7.setOnClickListener(new NumberPadClickListener(7));
+        mTvNumber8.setOnClickListener(new NumberPadClickListener(8));
+        mTvNumber9.setOnClickListener(new NumberPadClickListener(9));
+        mTvNumber0.setOnClickListener(new NumberPadClickListener(0));
+        mTvNumber00.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mAmount = (mAmount == 0 ? 0 : mAmount * 100);
+                        mTvAmount.setText(StringUtils.makeNumberComma(mAmount));
+                    }
+                }
+        );
+        mTvNumberC.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mAmount = 0;
+                        mTvAmount.setText(StringUtils.makeNumberComma(mAmount));
+                    }
+                }
+        );
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    class NumberPadClickListener implements View.OnClickListener {
+        long number;
+
+        NumberPadClickListener(long number) {
+            this.number = number;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mAmount = (mAmount == 0 ? number : mAmount * 10 + number);
+            mTvAmount.setText(StringUtils.makeNumberComma(mAmount));
+        }
     }
 }
